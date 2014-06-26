b85a5e4562b4e467c69a7c0cf246a29d57aabe80
blackmaggot <blackmaggot666@gmail.com>
2014-06-26 08:05:31 +0200

    dodano brakujaca czesc z cwiczenia 5

--- exircises/fifth.exircise.py
+++ exircises/fifth.exircise.py
@@ -1,1 +1,18 @@
+# coding=utf-8
 __author__ = 'WiktorMarchewka'
+import math
+import cmath
+def quadratic(a,b,c):
+    delta=b**2-4*a*c
+    if delta <0:
+        x1=(-b+cmath.sqrt(delta))/2*a
+        x2=(-b-cmath.sqrt(delta))/2*a
+        return x1,x2
+    else:
+        x1=(-b+math.sqrt(delta))/2*a
+        x2=(-b-math.sqrt(delta))/2*a
+        return x1,x2
+#Przykłady
+print(quadratic(1,0,9))
+
+print(quadratic(1,0,4))
